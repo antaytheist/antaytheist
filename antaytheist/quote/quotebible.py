3575150500b7f3f016298b1cf5745faaca947ea1
antaytheist <antaytheistowner@zoho.com>
2016-04-07 18:43:27 +1000

    Added GETBIBLE.net as primary source, with BibleGateway as fallback

--- antaytheist/quote/quotebible.py
+++ antaytheist/quote/quotebible.py
@@ -1,27 +1,19 @@
 # bible quoter for antaytheist
+import json
 import requests
 from bs4 import BeautifulSoup
 
 from ..util.bible import bibfullref
 
-def quote(args):
-    ref = bibfullref(args[0])
-    if ref["success"] == False:
-        return ref
-
+def biblegateway(ref):
     searchstr = ref["book"] + " " + ref["chapter"]
     if "versestart" in ref:
         searchstr += ":" + ref["versestart"]
     if "verseend" in ref:
         searchstr += "-" + ref["verseend"]
 
-    if len(args) == 2:
-        bibversion = args[1]
-    else:
-        bibversion = "KJV"
-
     try:
-        gatewayget = requests.get("https://www.biblegateway.com/passage/", params={"search": searchstr, "version": bibversion}, timeout=15)
+        gatewayget = requests.get("https://www.biblegateway.com/passage/", params={"search": searchstr, "version": ref["version"]}, timeout=15)
     except requests.exceptions.RequestException:
         return {"success": False, "error": "Could not connect to BibleGateway"}
 
@@ -31,8 +23,8 @@ def quote(args):
         return {"success": False, "error": "Passage not found"}
 
     output = {
-        "ref": datacontainer.find(class_="passage-display-version").string,
-        "version": datacontainer.find(class_="passage-display-bcv").string,
+        "version": datacontainer.find(class_="passage-display-version").string,
+        "ref": datacontainer.find(class_="passage-display-bcv").string,
         "text": ""
     }
 
@@ -40,4 +32,56 @@ def quote(args):
         for string in verse.strings:
             output["text"] += string
 
-    return {"success": True, "refdata": ref, "output": output}
+    return {"success": True, "refdata": ref, "output": output, "source": {"name": "BibleGateway", "url": gatewayget.url}}
+
+def getbible(ref):
+    searchstr = ref["book"] + " " + ref["chapter"]
+    if "versestart" in ref:
+        searchstr += ":" + ref["versestart"]
+    if "verseend" in ref:
+        searchstr += "-" + ref["verseend"]
+
+    try:
+        getbibleget = requests.get("https://getbible.net/json", params={"p": searchstr, "v": ref["version"]}, timeout=15)
+    except requests.exceptions.RequestException:
+        return {"success": False, "error": "Could not connect to GETBIBLE.net"}
+
+    try:
+        response = json.loads(getbibleget.text[1:-2])
+
+        output = {
+            "ref": searchstr,
+            "version": response["version"],
+            "text": ""
+        }
+
+        print(response["book"][0]["chapter"])
+        for versenum, verseitem in response["book"][0]["chapter"].items():
+            output["text"] += versenum + verseitem["verse"] + " "
+
+    except Exception: # would be nice to narrow this down
+        return {"success": False, "error": "Invalid data returned from GETBIBLE.net: possibly invalid passage given"}
+
+    return {"success": True, "refdata": ref, "output": output, "source": {"name": "GETBIBLE.net", "url": getbibleget.url}}
+
+biblesources = [
+    getbible,
+    biblegateway,
+]
+
+def quote(args):
+    ref = bibfullref(args[0])
+    if ref["success"] == False:
+        return ref
+
+    if len(args) == 2:
+        ref["version"] = args[1]
+    else:
+        ref["version"] = "KJV"
+
+    for source in biblesources:
+        output = source(ref)
+        if output["success"]:
+            break
+
+    return output
