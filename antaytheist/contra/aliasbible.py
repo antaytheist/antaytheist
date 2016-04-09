0bdb9f8cfc01d4662d368e53be1a956d37dfbfaf
antaytheist <antaytheistowner@zoho.com>
2016-04-09 19:44:30 +1000

    Added contra bible aliases

---
+++ antaytheist/contra/aliasbible.py
@@ -0,0 +1,17 @@
+# bible contradiction aliases
+
+aliases = {
+    "creation": "Genesis 1:1",
+    "tempt": "Genesis 22:1",
+    "faith": "Galatians 2:16",
+    "face": "Genesis 12:7",
+    "seen": "Genesis 12:7",
+    "evil": "Isaiah 45:7",
+    "ascend": "2 Kings 2:11",
+    "tire": "Isaiah 40:28",
+    "tired": "Isaiah 40:28",
+    "lie": "Jeremiah 20:7",
+    "tell": "Mark 16:8",
+    "easter": "Mark 16:8",
+    
+}
--- antaytheist/contra/contrabible.py
+++ antaytheist/contra/contrabible.py
@@ -6,11 +6,16 @@ from bs4 import BeautifulSoup
 from ..util.bible import bibfullref
 from ..util.scripture import chapterverseregex
 from ..util.sab import book2url
+from .aliasbible import aliases
 
 # Screw fancy output, just give them a link
 
 def contra(args):
-    ref = bibfullref(args[0])
+    if args[0].lower() in aliases:
+        ref = bibfullref(aliases[args[0]])
+    else:
+        ref = bibfullref(args[0])
+
     if ref["success"] == False:
         return ref
 
@@ -21,8 +26,12 @@ def contra(args):
     except requests.exceptions.RequestException:
         return {"success": False, "error": "Could not connect to SAB"}
 
-    # a stray tag must be removed, otherwise bs gets confused
-    soup = BeautifulSoup(re.sub(r"manger\?</b>", "", sabget.text), "html.parser")
+    # a stray tag must be removed, otherwise parser/bs gets confused
+    finaltext = re.sub(r"manger\?</b>", "manger?", sabget.text)
+    if ref["book"] == "Genesis":
+        # yet another stray tag
+        finaltext = re.sub(r"1:29</a>, </A>", "1:29</a>, ", finaltext)
+    soup = BeautifulSoup(finaltext, "html.parser")
 
     if "versestart" in ref:
         # ensure that we only get an exact match for the chapter:verse we are looking for
@@ -30,8 +39,8 @@ def contra(args):
         searchref = ref["chapter"] + ":" + ref["versestart"] + r"(?!\d)"
     else:
         searchref = ref["chapter"] + r":\d{1,3}"
+
     foundref = soup.find("ol").find("a", string=re.compile(searchref))
-    print(foundref)
     if foundref == None:
         return {"success": False, "error": "Could not find a relevant contradiction"}
 
